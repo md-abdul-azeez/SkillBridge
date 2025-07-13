1af3fef0152c6b9ba6dac3bd46db43f9bc5c3dde
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-07-13 23:25:50 +0530

    Cleaned up history

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-dotenv.config({});
 const app = express();
 
 app.use(express.json());
@@ -15,8 +12,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, ()=>{
-    connectDB();
     console.log(`Server is listening on ${PORT}`);
 })
