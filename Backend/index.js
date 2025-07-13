b9fbf10d9df98e87e5fcbed4224b997e5f618d7e
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-07-14 00:09:04 +0530

    connected database

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 const app = express();
 
 app.use(express.json());
@@ -12,7 +15,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
+    connectDB();
     console.log(`Server is listening on ${PORT}`);
 })
