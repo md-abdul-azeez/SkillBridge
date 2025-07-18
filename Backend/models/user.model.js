cc6bf619dd58865a4750d65984a98ec5c87ad78e
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-07-18 23:24:48 +0530

    setup user controllers

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
         required : true,
         unique : true
     },
-    phoneNUmber : {
+    phoneNumber : {
         type : Number,
         required : true
     },
