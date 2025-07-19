bebb6a07333ee98af184d223b2993ec2d15d0203
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-07-19 23:35:51 +0530

    created company routes

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js"
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 dotenv.config({});
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
 
 app.listen(PORT, ()=>{
     connectDB();
