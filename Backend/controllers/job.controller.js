000f3c377607aa1e6bd412240c93ce4f60b191ea
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-07-22 22:51:36 +0530

    setup job controllers

--- Backend/controllers/job.controller.js
+++ Backend/controllers/job.controller.js
@@ -55,3 +55,51 @@ export const getAllJobs = async (req, res) => {
     });
   };
 };
+export const getJobById = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId).populate({
+      path : "applications"
+    });
+    if (!job) {
+      return res.status(404).json({
+        message : "Jobs not found",
+        success : false
+      });
+    };
+    return res.status(200).json({
+      job,
+      success : true
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error : error.message,
+      success : false
+    });
+  }
+};
+export const getAdminJobs = async (req, res) => {
+  try {
+    const adminId = req.id;
+    const jobs = await Job.find({ created_by : adminId }).populate({
+      path : 'company'
+    });
+    if (!jobs) {
+      return res.status(404).json({
+        message : "Jobs not found",
+        success : false
+      });
+    };
+    return res.status(200).json({
+      jobs,
+      success : true
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error : error.message,
+      success : false,
+    });
+  }
+};
