467b847b7931712353633b9f35914d7938e4e55b
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-07-22 23:04:05 +0530

    created user routes

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile);
 
 export default router;
