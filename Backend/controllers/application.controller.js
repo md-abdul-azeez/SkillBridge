e60f1aeca9da0135c330fa99d5cbb96fa5c6d406
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-07-24 23:21:18 +0530

    setup application controllers

--- Backend/controllers/application.controller.js
+++ Backend/controllers/application.controller.js
@@ -43,7 +43,7 @@ export const applyJob = async (req, res) => {
         });
     }
 };
-export const getAppliedJobs = async (req,res) => {
+export const getAppliedJobs = async (req, res) => {
     try {
         const userId = req.id;
         const application = await Application.find({applicant : userId}).sort({createdAt : -1}).populate({
@@ -65,6 +65,58 @@ export const getAppliedJobs = async (req,res) => {
             application,
             success : true
         });
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const getApplicants = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path : 'applications',
+            options : {sort : {createdAt : -1}},
+            populate : {
+                path :'applicant'
+            }
+        });
+        if(!job){
+            return res.status(404).json({
+                message : 'Job not found',
+                success : false
+            });
+        };
+        return res.status(200).json({
+            job, 
+            succees : true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const updateStatus = async (req, res) => {
+    try {
+        const {status} = req.body;
+        const applicationId = req.params.id;
+        if(!status){
+            return res.status(400).json({
+                message : 'status is required',
+                success : false
+            });
+        };
+        const application = await Application.findOne({_id : applicationId});
+        if(!application){
+            return res.status(404).json({
+                message : "Application not found.",
+                success : false
+            });
+        };
+        application.status = status.toLowerCase();
+        await application.save();
+        return res.status(200).json({
+            message : "Status updated successfully.",
+            success : true
+        });
+
     } catch (error) {
         console.log(error);
     }
