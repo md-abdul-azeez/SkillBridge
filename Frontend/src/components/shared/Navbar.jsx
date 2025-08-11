5af262689955e06ab209f22ba2834fa08239e2d2
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-08-11 23:02:06 +0530

    updated navbar

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='bg-white'>
+    <div className='bg-white flex items-center justify-between mx-auto max-w-7xl h-16'>
       <div>
         <h1 className='text-2xl font-bold'>Skill<span className='text-[#F83002]'>Bridge</span></h1>
       </div>
       <div>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul className='flex font-medium gap-5'>
+          <li>Home</li>
+          <li>Jobs</li>
+          <li>Browse</li>
         </ul>
       </div>
     </div>
