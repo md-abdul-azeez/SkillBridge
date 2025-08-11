90360466e75a57beafe2c9b3c16305506d7e0c34
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-08-11 19:20:24 +0530

    installed dependencies

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+     <h1 className='bg-red-500'>Abdul</h1>
+     <Button>Click</Button>
+    </>
+  )
+}
+export default App
