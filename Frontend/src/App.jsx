fc1bd15231a7a57793bd68cc759bff160b25ef94
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-08-11 22:07:38 +0530

    created navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
-import { Button } from "./components/ui/button";
+import Navbar from './components/shared/Navbar';
 
 function App() {
   return (
     <>
-     <h1 className='bg-red-500'>Abdul</h1>
-     <Button>Click</Button>
+      <Navbar/>
     </>
   )
 }
---
+++ Frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='bg-white'>
+      <div>
+        <h1 className='text-2xl font-bold'>Skill<span className='text-[#F83002]'>Bridge</span></h1>
+      </div>
+      <div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
