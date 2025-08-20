db456346db87d77a2e3283cd25d309dd6472e9ef
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-08-20 23:56:51 +0530

    updated auth

--- Frontend/src/components/auth/Login.jsx
+++ Frontend/src/components/auth/Login.jsx
@@ -1,45 +1,87 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup } from "../ui/radio-group";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
 
-const [input, setInput] = useState({
-  fullName: "", email: "", phoneNumber: "", password: "", role: "", file: ""
-});
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
 
-const changeEventHandler = (e) => {
-  setInput(...input, [e.target.name: ])
-}
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="flex items-center justify-center max-w-7xl mx-auto">
-      <form className="w-1/2 border border-gray-200 rounded-md p-4 my-10">
+      <form
+        onSubmit={submitHandler}
+        className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+      >
         <h1 className="font-bold text-xl mb-5">Login</h1>
         <div className="my-2">
           <Label className="mb-3">Email</Label>
-          <Input type="email" placeholder="username@provider.com" />
+          <Input
+            type="email"
+            placeholder="username@provider.com"
+            value={input.email}
+            name="email"
+            onChange={changeEventHandler}
+          />
         </div>
         <div className="my-2">
           <Label className="mb-3">Password</Label>
-          <Input type="password" placeholder="password" />
+          <Input
+            type="password"
+            value={input.password}
+            name="password"
+            onChange={changeEventHandler}
+            placeholder="password"
+          />
         </div>
         <div className="flex justify-between">
           <RadioGroup className="flex items-center my-2 gap-4">
             <div className="flex items-center space-x-2">
-              <Input type="radio" name="role" value="student" />
+              <Input
+                type="radio"
+                name="role"
+                value="student"
+                checked={input.role === "student"}
+                onChange={changeEventHandler}
+              />
               <Label htmlFor="option-one">Student</Label>
             </div>
             <div className="flex items-center space-x-2">
-              <Input type="radio" name="role" value="recruiter" />
+              <Input
+                type="radio"
+                name="role"
+                value="recruiter"
+                checked={input.role === "recruiter"}
+                onChange={changeEventHandler}
+              />
               <Label htmlFor="option-two">Recruiter</Label>
             </div>
           </RadioGroup>
         </div>
-        <Button type="submit" className="my-3 bg-blue-600 text-white cursor-pointer">Login</Button>
-        <span className="mx-5">Don't have an account? <Link to="/signup" className="text-blue-500 mx-3">Signup</Link></span>
+        <Button
+          type="submit"
+          className="my-3 bg-blue-600 text-white cursor-pointer"
+        >
+          Login
+        </Button>
+        <span className="mx-5">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500 mx-3">
+            Signup
+          </Link>
+        </span>
       </form>
     </div>
   );
--- Frontend/src/components/auth/Signup.jsx
+++ Frontend/src/components/auth/Signup.jsx
@@ -1,48 +1,124 @@
 import React from "react";
+import { useState } from "react";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup } from "../ui/radio-group";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 const Signup = () => {
+  const [input, setInput] = useState({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="flex items-center justify-center max-w-7xl mx-auto">
-      <form className="w-1/2 border border-gray-200 rounded-md p-4 my-10">
+      <form
+        onSubmit={submitHandler}
+        className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+      >
         <h1 className="font-bold text-xl mb-5">Sign Up</h1>
         <div className="my-2 mb-3">
           <Label className="mb-3">Full Name</Label>
-          <Input type="text" placeholder="Mohammed Abdul Azeez" />
+          <Input
+            type="text"
+            value={input.fullName}
+            name="fullName"
+            onChange={changeEventHandler}
+            placeholder="Mohammed Abdul Azeez"
+          />
         </div>
         <div className="my-2">
           <Label className="mb-3">Phone Number</Label>
-          <Input type="number" placeholder="+91 9301233280" />
+          <Input
+            type="number"
+            value={input.phoneNumber}
+            name="phoneNumber"
+            onChange={changeEventHandler}
+            placeholder="+91 9301233280"
+          />
         </div>
         <div className="my-2">
           <Label className="mb-3">Email</Label>
-          <Input type="email" placeholder="username@provider.com" />
+          <Input
+            type="email"
+            value={input.email}
+            name="email"
+            onChange={changeEventHandler}
+            placeholder="username@provider.com"
+          />
         </div>
         <div className="my-2">
           <Label className="mb-3">Password</Label>
-          <Input type="password" placeholder="password" />
+          <Input
+            type="password"
+            value={input.password}
+            name="password"
+            onChange={changeEventHandler}
+            placeholder="password"
+          />
         </div>
         <div className="flex justify-between">
           <RadioGroup className="flex items-center my-2 gap-4">
             <div className="flex items-center space-x-2">
-              <Input type="radio" name="role" value="student" />
+              <Input
+                type="radio"
+                name="role"
+                value="student"
+                checked={input.role === "student"}
+                onChange={changeEventHandler}
+              />
               <Label htmlFor="option-one">Student</Label>
             </div>
             <div className="flex items-center space-x-2">
-              <Input type="radio" name="role" value="recruiter" />
+              <Input
+                type="radio"
+                name="role"
+                value="recruiter"
+                checked={input.role === "recruiter"}
+                onChange={changeEventHandler}
+              />
               <Label htmlFor="option-two">Recruiter</Label>
             </div>
           </RadioGroup>
         </div>
         <div className="">
           <Label className="mb-3">Profile</Label>
-          <Input type="file" accept="image/*" className="cursor-pointer"/>
+          <Input
+            type="file"
+            accept="image/*"
+            className="cursor-pointer"
+            onChange={changeFileHandler}
+          />
         </div>
-        <Button type="submit" className="my-3 bg-blue-600 text-white cursor-pointer">Signup</Button>
-        <span className="mx-5">Already have an account? <Link to="/login" className="text-blue-500 mx-3">Login</Link></span>
+        <Button
+          type="submit"
+          className="my-3 bg-blue-600 text-white cursor-pointer"
+        >
+          Signup
+        </Button>
+        <span className="mx-5">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-500 mx-3">
+            Login
+          </Link>
+        </span>
       </form>
     </div>
   );
