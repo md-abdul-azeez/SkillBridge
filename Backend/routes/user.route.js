d80f285a9292aa53236f705a1160d4f4e7189a86
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-08-26 22:15:59 +0530

    installed multer

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -3,7 +3,7 @@ import {register, updateProfile} from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile);
