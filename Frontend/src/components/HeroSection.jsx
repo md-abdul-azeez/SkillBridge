3fff8a834aa9f50ea067f8bc4da2d0c3d3cb0d7d
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-08-28 23:09:06 +0530

    created HeroSection

---
+++ Frontend/src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div className='text-center'>
+        <span className='px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 job hunt website</span>
+        <h1 className='text-5xl font-bold'>Search, Apply & <br/> get your <span className='text-[#6A38C2]'>Dream Job</span></h1>
+    </div>
+  );
+};
+
+export default HeroSection;
