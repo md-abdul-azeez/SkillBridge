4455253a224dedb17e82d2115d3322870e9ea3c1
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-08-29 23:31:03 +0530

    updated HeroSection

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className='text-center'>
-        <span className='px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 job hunt website</span>
-        <h1 className='text-5xl font-bold'>Search, Apply & <br/> get your <span className='text-[#6A38C2]'>Dream Job</span></h1>
+    <div className="text-center">
+      <div className="flex flex-col gap-5 my-10">
+        <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium">
+          No. 1 job hunt website
+        </span>
+        <h1 className="text-5xl font-bold">
+          Search, Apply & <br /> get your{" "}
+          <span className="text-[#6A38C2]">Dream Job</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cum esse, minus mollitia sint asperiores.
+        </p>
+        <div className="flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto">
+          <input type="text" placeholder="find your dream job" className="outline-none border-none w-full "/>
+        </div>
+      </div>
     </div>
   );
 };
