48cc02aba104f4e166c8771e888c57e0d4b68993
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-08-30 23:14:17 +0530

    designed HeroSection

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -16,6 +16,9 @@ const HeroSection = () => {
         </p>
         <div className="flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto">
           <input type="text" placeholder="find your dream job" className="outline-none border-none w-full "/>
+          <Button className="rounded-r-full bg-[#6A38C2]">
+            <Search className="h-5 w-5"/>
+          </Button>
         </div>
       </div>
     </div>
