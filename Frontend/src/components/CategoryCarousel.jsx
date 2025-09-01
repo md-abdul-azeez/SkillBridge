90aebfe5e2df8a40e290fb57636868ed847fa792
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-09-01 22:54:43 +0530

    designed CategoryCarousel

--- Frontend/src/components/CategoryCarousel.jsx
+++ Frontend/src/components/CategoryCarousel.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import { Carousel, CarouselContent, CarouselItem } from './ui/carousel';
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Button } from "./ui/button";
 const CategoryCarousel = () => {
-    const category = [
-        "Frontend Developer",
-        "Backend Developer",
-        "FullStack Developer",
-        "Network Engineer",
-        "Graphic Designer"
-    ];
+  const category = [
+    "Frontend Developer",
+    "Backend Developer",
+    "FullStack Developer",
+    "Network Engineer",
+    "Graphic Designer",
+  ];
   return (
     <div>
-      <Carousel>
+      <Carousel className="w-full max-w-xl mx-auto my-20">
         <CarouselContent>
-          <CarouselItem>
-            
-          </CarouselItem>
+          {category.map((cat, idx) => {
+            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+              <Button variant="outline" className="rounded-full">
+                {cat}
+              </Button>
+            </CarouselItem>;
+          })}
+          <CarouselItem></CarouselItem>
         </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
       </Carousel>
     </div>
   );
