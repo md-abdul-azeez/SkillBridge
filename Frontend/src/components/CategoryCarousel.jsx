19675b61ae13e78a6581fc66edb9246b6eab002e
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-09-03 23:49:44 +0530

    designed CategoryCarousel

--- Frontend/src/components/CategoryCarousel.jsx
+++ Frontend/src/components/CategoryCarousel.jsx
@@ -19,7 +19,7 @@ const CategoryCarousel = () => {
     <div>
       <Carousel className="w-full max-w-xl mx-auto my-20">
         <CarouselContent>
-          {category.map((cat, idx) => {
+          {category.map((cat) => {
             <CarouselItem className="md:basis-1/2 lg-basis-1/3">
               <Button variant="outline" className="rounded-full">
                 {cat}
