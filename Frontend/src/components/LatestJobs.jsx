c8a3933b74119c826fe43616df123c97b10fbf1a
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-09-04 23:18:44 +0530

    build LatestJobs

---
+++ Frontend/src/components/LatestJobs.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const LatestJobs = () => {
+  return (
+    <div className='max-w-7xl mx-auto my-20'>
+        <h1 className='text-4xl font-bold'><span className='text-[#F83002]'>Latest & Top </span>Job Openings</h1>
+    </div>
+  );
+};
+export default LatestJobs;
