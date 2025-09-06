bee5fd4547c997d704bc0e0462d48b4c6309107f
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-09-06 23:18:52 +0530

    build LatestJobs

--- Frontend/src/components/LatestJobCards.jsx
+++ Frontend/src/components/LatestJobCards.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
+import React from 'react';
 const LatestJobCards = () => {
   return (
     <div>LatestJobCards</div>
-  )
-}
-
-export default LatestJobCards
+  );
+};
+export default LatestJobCards;
