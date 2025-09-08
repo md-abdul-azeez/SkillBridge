657aab00117938d1be65760403fb749db30c5f42
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-09-08 23:57:34 +0530

    updated LatestJobCards

--- Frontend/src/components/LatestJobCards.jsx
+++ Frontend/src/components/LatestJobCards.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 const LatestJobCards = () => {
   return (
-    <div>LatestJobCards</div>
+    <div>
+      <div>
+        <h1>Company Name</h1>
+        <p>India</p>
+      </div>
+    </div>
   );
 };
 export default LatestJobCards;
