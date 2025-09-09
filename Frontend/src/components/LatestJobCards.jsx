4cc4e7f54b3147bb39d38f8de43a27a48149f85a
md-abdul-azeez <mdabdulazeez457@gmail.com>
2025-09-09 23:56:39 +0530

    added badge

--- Frontend/src/components/LatestJobCards.jsx
+++ Frontend/src/components/LatestJobCards.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Badge } from './ui/badge';
 const LatestJobCards = () => {
   return (
-    <div>
+    <div className=''>
       <div>
         <h1>Company Name</h1>
         <p>India</p>
+      </div>
+      <div>
+        <h1>Job Title</h1>
+        <p>Job description</p>
+      </div>
+      <div>
+        <Badge>Positions</Badge>
       </div>
     </div>
   );
